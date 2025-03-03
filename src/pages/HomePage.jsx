1b4176c5f0971d1a069c83346c6709fe4053c74f
MatheoKatbie <matheo.katbie@aurera.fr>
2025-03-03 11:29:54 +0100

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -18,7 +18,7 @@ const HomePage = () => {
     return (
         <>
             <PageTitle
-                title="Accueil | Lodgic - Solutions Digitales Innovantes"
+                title="Accueil | Lodgic - Solutions Digitales"
                 description="Lodgic vous accompagne dans votre transformation digitale avec des solutions sur mesure. Experts en développement web, applications mobiles et stratégie digitale pour propulser votre entreprise vers le succès. Découvrez nos services, notre processus et nos réalisations."
             />
             <div className="pt-16">
