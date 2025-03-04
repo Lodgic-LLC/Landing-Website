3fb1d58617ee8aefb4f9151edcc1ddae70cedeee
MatheoKatbie <matheo.okatbie@icloud.com>
2025-03-04 18:12:31 +0100

    delete contact

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
-import FAQ from "@/components/pages/home/FAQ";
-import Features from "@/components/pages/home/Features";
-import Hero from "@/components/pages/home/Hero";
-import Process from "@/components/pages/home/Process";
-import ProjectCTA from "@/components/pages/home/ProjectCTA";
-import SuccessStories from "@/components/pages/home/SuccessStories";
-import { Metadata } from "next";
+import FAQ from '@/components/pages/home/FAQ'
+import Features from '@/components/pages/home/Features'
+import Hero from '@/components/pages/home/Hero'
+import Process from '@/components/pages/home/Process'
+import ProjectCTA from '@/components/pages/home/ProjectCTA'
+import SuccessStories from '@/components/pages/home/SuccessStories'
+import { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: "Lodgic - Accueil",
-  description: "Lodgic est une agence de développement web et mobile",
-};
+  title: 'Lodgic - Accueil',
+  description: 'Lodgic est une agence de développement web et mobile',
+}
 
 export default function Home() {
-
   return (
     <div className="pt-16">
       <Hero />
@@ -22,5 +21,5 @@ export default function Home() {
       <SuccessStories />
       <FAQ />
     </div>
-  );
+  )
 }
