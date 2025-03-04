98ef073b22dea34fcc8a54dcc8022009899f4c5f
MatheoKatbie <matheo.okatbie@icloud.com>
2025-03-04 18:00:34 +0100

    update google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,26 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Footer from "@/components/layout/Footer";
-import Navbar from "@/components/layout/Navbar";
-import AOSInitializer from "@/components/layout/AOSInitializer";
-import Script from "next/script";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Footer from '@/components/layout/Footer'
+import Navbar from '@/components/layout/Navbar'
+import AOSInitializer from '@/components/layout/AOSInitializer'
+import Script from 'next/script'
 
 const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-  weight: ["400", "500", "600", "700"],
-});
+  subsets: ['latin'],
+  variable: '--font-inter',
+  weight: ['400', '500', '600', '700'],
+})
 
 export const metadata: Metadata = {
-  title: "Lodgic",
-  description: "Lodgic est une agence de développement web et mobile",
-};
+  title: 'Lodgic',
+  description: 'Lodgic est une agence de développement web et mobile',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="fr" className="overflow-x-hidden">
@@ -32,42 +32,27 @@ export default function RootLayout({
             _iub.csConfiguration = {"siteId":3950256,"cookiePolicyId":94594353,"lang":"fr","storage":{"useSiteId":true}};
           `}
         </Script>
-        <Script
-          src="https://cs.iubenda.com/autoblocking/3950256.js"
-          strategy="beforeInteractive"
-        />
-        <Script
-          src="//cdn.iubenda.com/cs/gpp/stub.js"
-          strategy="beforeInteractive"
-        />
-        <Script
-          src="//cdn.iubenda.com/cs/iubenda_cs.js"
-          strategy="afterInteractive"
-          charSet="UTF-8"
-        />
+        <Script src="https://cs.iubenda.com/autoblocking/3950256.js" strategy="beforeInteractive" />
+        <Script src="//cdn.iubenda.com/cs/gpp/stub.js" strategy="beforeInteractive" />
+        <Script src="//cdn.iubenda.com/cs/iubenda_cs.js" strategy="afterInteractive" charSet="UTF-8" />
 
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-1S4DK5T3RT"
-          strategy="afterInteractive"
-        />
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-LV93937W8D" strategy="afterInteractive" />
         <Script id="google-analytics" strategy="afterInteractive">
           {`
-          window.dataLayer = window.dataLayer || [];
+         window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-1S4DK5T3RT');
+
+          gtag('config', 'G-LV93937W8D');
         `}
         </Script>
-
       </head>
-      <body
-        className={`${inter.variable} bg-white min-h-screen overflow-x-hidden antialiased`}
-      >
+      <body className={`${inter.variable} bg-white min-h-screen overflow-x-hidden antialiased`}>
         <AOSInitializer />
         <Navbar />
         {children}
         <Footer />
       </body>
     </html>
-  );
+  )
 }
