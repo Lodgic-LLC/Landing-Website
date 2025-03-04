d00991627e0b9664d5a6c621427a94b77559c3ae
MatheoKatbie <matheo.katbie@aurera.fr>
2025-03-04 10:49:26 +0100

    update pipeline

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  swcMinify: true,
+  reactStrictMode: true,
+  poweredByHeader: false,
+  compress: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    minimumCacheTTL: 60,
+  },
+  experimental: {
+    optimizeCss: true,
+  },
 };
 
 export default nextConfig;
